8eb61fac61655647bbc639e47e95b36712c31621
Ben Johns <benjohns.git.gpg.donotreply@gmail.com>
2019-05-05 11:18:53 -0600

    added empty schedule use case methods in prep for implementation

--- internal/usecase/schedule.go
+++ internal/usecase/schedule.go
@@ -20,3 +20,38 @@ type ScheduleRepo interface {
 	Add(*schedule.Schedule) (TaskID, Error)
 	Update(ScheduleID, *schedule.Schedule) Error
 }
+
+// GetSchedule returns a single schedule
+func GetSchedule(r ScheduleRepo, id ScheduleID) (*ScheduleData, Error) {
+	return nil, NewError(ErrUnknown, "not implemented")
+}
+
+// ListSchedules returns all schedules
+func ListSchedules(r ScheduleRepo) (map[ScheduleID]*schedule.Schedule, Error) {
+	return map[ScheduleID]*schedule.Schedule{}, NewError(ErrUnknown, "not implemented")
+}
+
+// AddSchedule adds a new schedule
+func AddSchedule(r ScheduleRepo, s *schedule.Schedule) (ScheduleID, Error) {
+	return 0, NewError(ErrUnknown, "not implemented")
+}
+
+// PauseSchedule pauses the schedule
+func PauseSchedule(r ScheduleRepo, id ScheduleID) Error {
+	return NewError(ErrUnknown, "not implemented")
+}
+
+// UnpauseSchedule unpauses the schedule
+func UnpauseSchedule(r ScheduleRepo, id ScheduleID) Error {
+	return NewError(ErrUnknown, "not implemented")
+}
+
+// AddRecurringTask adds a new recurring task to the schedule
+func AddRecurringTask(r ScheduleRepo, id ScheduleID, rt *schedule.RecurringTask) Error {
+	return NewError(ErrUnknown, "not implemented")
+}
+
+// RemoveRecurringTask removes the recurring task at the specified index from the schedule
+func RemoveRecurringTask(r ScheduleRepo, id ScheduleID, taskIndex int) Error {
+	return NewError(ErrUnknown, "not implemented")
+}
